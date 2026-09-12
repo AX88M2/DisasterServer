8bd9f8d7a9c925c239c25e613463590f26144d4e
FakerNull0 <twopassion790@gmail.com>
2026-09-12 15:27:52 +0300

    Update anticheat

--- disasterserver/states/Game.c
+++ disasterserver/states/Game.c
@@ -1024,6 +1024,12 @@ bool game_state_handletcp(PeerData* v, Packet* packet)
 				break;
 			}
 
+			if (v->server->game.bring_state != BS_ACTIVATED)
+			{
+				Warn("%s (id %d) this bro escaped before ring activated", v->nickname.value, v->id);
+				AssertOrDisconnect(v->server, false);
+			}
+
 			if (v->plr.flags & PLAYER_DEAD || v->plr.flags & PLAYER_DEMONIZED)
 				break;
 
