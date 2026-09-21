b94ac16961c7d4fdaf53c390087282b72fda1063
MilesGlitch <MilesGlitch@proton.me>
2026-09-21 12:15:41 +0300

    fix

--- disasterserver/States/LobbyState.cpp
+++ disasterserver/States/LobbyState.cpp
@@ -365,7 +365,7 @@ bool LobbyState::cmdHandle(Client &client, clientId pid, commandHash hash, std::
 #if _WIN32
             if (sscanf_s(message.c_str(), ".map %d", &requested) != 1) {
 #else
-            if (sscanf_r(message.c_str(), ".map %d", &requested) != 1) {
+            if (sscanf(message.c_str(), ".map %d", &requested) != 1) {
 #endif
                 this->server.sendMessage(client, "{}example:~ .map 1", CLRCODE_RED);
                 break;
